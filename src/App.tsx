5a37cca74028413f14195195bad65e98b9afe213
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-10 23:39:28 +0900

    carousel 1

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="slider">
       <a href="#slide-1">1</a>
       <a href="#slide-2">2</a>
       <a href="#slide-3">3</a>
       <a href="#slide-4">4</a>
       <a href="#slide-5">5</a>
 
-      <div>
+      <div className="slides">
         <div id="slide-1">1</div>
         <div id="slide-2">2</div>
         <div id="slide-3">3</div>
