2f421051707fe72bd06851f3265ba8456644b3aa
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-10 21:38:30 +0900

    divs only

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function App() {
-  return <div>empty application</div>;
+  return (
+    <div>
+      <a href="#slide-1">1</a>
+      <a href="#slide-2">2</a>
+      <a href="#slide-3">3</a>
+      <a href="#slide-4">4</a>
+      <a href="#slide-5">5</a>
+
+      <div>
+        <div id="slide-1">1</div>
+        <div id="slide-2">2</div>
+        <div id="slide-3">3</div>
+        <div id="slide-4">4</div>
+        <div id="slide-5">5</div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
