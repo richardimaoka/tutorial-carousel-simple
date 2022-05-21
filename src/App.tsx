fac9fb289342ec2c60dd08dcd780f090587bfc4f
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-21 14:55:29 +0900

    images added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 function App() {
-  return <div>empty application</div>;
+  return (
+    <div>
+      <div style={{ padding: "4px" }}>
+        <img
+          width="640px"
+          height="360px"
+          style={{ verticalAlign: "middle" }}
+          src="/images/1.png"
+        />
+      </div>
+      <div style={{ padding: "4px" }}>
+        <img
+          width="640px"
+          height="360px"
+          style={{ verticalAlign: "middle" }}
+          src="/images/2.png"
+        />
+      </div>
+      <div style={{ padding: "4px" }}>
+        <img
+          width="640px"
+          height="360px"
+          style={{ verticalAlign: "middle" }}
+          src="/images/3.png"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
