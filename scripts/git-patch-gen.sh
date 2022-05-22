a3a9bd00d59ebe7f054e3a06e86f874e28790ba9
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-05-22 22:59:32 +0900

    patches

--- scripts/git-patch-gen.sh
+++ scripts/git-patch-gen.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")" || exit
 cd ../ || exit # cd to the git repository root
 
 git log --oneline --reverse 8587d56..b9d3006 | \
-   sed -r 's/(^\w+\b)/git show --src-prefix=a\/client\/ --dst-prefix=b\/client\/ --patch \1 -- patches\/\1.patch/'
+   sed -r 's/(^\w+\b)(.*)/git show --binary --src-prefix=a\/client\/ --dst-prefix=b\/client\/ --patch \1 > patches\/\1.patch/'
    
